a7e1511cea9a37b6303576e9c3730b07b3be2daa
janinefelten <janine.felten92@gmail.com>
2024-08-21 11:30:51 +0200

    Add initial setup for writing first unittest

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# kata-testing-intro
+
+<img width="100px" src="typescript-original.svg" /></a>
+# Kata-Testing-Intro: Typescript
+
+
+## Test Command
+
+```sh
+$ npm test
+```
+
+## Task
+
+### 1. Unit Testing
+Go to arithmetic.test.ts and implement the tests for the arithmetic functions.
+Start with implementing the tests for the sum function.
+Then think of test cases for the divide function and implement them
+
+### 2. Integration Testing
+tbd.
---
+++ arithmetic.test.ts
@@ -0,0 +1,20 @@
+import {sum} from "./arithmetic";
+
+describe('sum function', () => {
+    it('should return the sum of two numbers', () => {
+        expect(sum(1, 2)).toBe(3);
+    });
+
+    it('should return a negative number when summing two negative numbers', () => {
+    });
+
+    it('should handle zero correctly', () => {
+    });
+
+    it('should handle adding zero to zero', () => {
+    });
+});
+
+describe('divide function', () => {
+
+});
